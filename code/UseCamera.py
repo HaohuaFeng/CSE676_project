a1d3555223f3f63af9a6a5ed70defc653e7afbf2
Haohua Feng <haohuafeng08@gmail.com>
2023-10-29 09:05:05 -0400

    adjust and test on camera

--- code/UseCamera.py
+++ code/UseCamera.py
@@ -3,12 +3,12 @@ from torchvision import transforms
 import cv2
 import platform
 from PIL import Image
-from models import VGG as model
+from models import VGG16 as model
 
 
 def generate_input_frame(frame):
-    grayscale_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)  # convert to grayscale
-    grayscale_frame = Image.fromarray(grayscale_frame)  # convert tp PIL image
+    # frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)  # convert to grayscale
+    grayscale_frame = Image.fromarray(frame)  # convert tp PIL image
     frame_transforms = transforms.Compose([
         transforms.Grayscale(num_output_channels=1),  # turn the graph to single color channel
         transforms.Resize((224, 224)),
@@ -18,25 +18,34 @@ def generate_input_frame(frame):
     return frame_transforms(grayscale_frame)
 
 
-def camera():
+def camera(width=1920, high=1080):
     device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
     print('Device:', device)
     print("\n** Press 'esc' to quit **\n")
 
+    font = cv2.FONT_HERSHEY_SIMPLEX
+    org = (20, 20)  # Position of the text
+    font_scale = 0.5
+    font_color = (0, 0, 255)  # BGR color (green in this case)
+    thickness = 1
+
     default_capture_device = 0
-    
     capture = cv2.VideoCapture(default_capture_device)
+    capture.set(cv2.CAP_PROP_FRAME_WIDTH, width)
+    capture.set(cv2.CAP_PROP_FRAME_HEIGHT, high)
     capture.open(0)  # turn on capture device
 
-    labels = ['angry', 'disgust', 'fear', 'happy', 'neutral', 'sad', 'surprise']
+    face_reg = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
 
-    m = model.VGG16(num_classes=7)
-    m.load_state_dict(torch.load("./model_data/VGG16/model.pth"))
+    m = model.EmotionCNN(num_classes=7)
+    m.load_state_dict(torch.load(model.pth_save_path))
     m.to(device)
     m.eval()
+    labels = ['angry', 'disgust', 'fear', 'happy', 'neutral', 'sad', 'surprise']
+
     with torch.no_grad():
         while capture.isOpened():
-            s, frame = capture.read()  # capture 1 frame
+            s, frame = capture.read()
             if not s:
                 print('Fail to capture, quit')
                 break
@@ -44,19 +53,38 @@ def camera():
                 print('Quit')
                 break
 
-            input_frame = generate_input_frame(frame).unsqueeze(0).to(device)
+            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+            face_image = None
+            faces = face_reg.detectMultiScale(gray, scaleFactor=1.25, minNeighbors=5, minSize=(48, 48))
+            for (x, y, w, h) in faces:
+                face_image = frame[y:y+h, x:x+w]
+                cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+                break
 
+            if face_image is not None:
+                input_frame = generate_input_frame(face_image).unsqueeze(0).to(device)
+                output = m(input_frame)
+                probability = torch.nn.functional.softmax(output, dim=1)
+                max_probability, prediction = torch.max(probability, dim=1)
+                to_decimal = ['{:.2f}'.format(float(item.item())) for item in probability[0]]
+                mapping = {labels[i]: to_decimal[i] for i in range(7)}
+                # print(f"Prediction: {labels[prediction]}, probability: {mapping}")
+                text = "|".join([f"{key}: {value}" for key, value in mapping.items()])
+                lines = text.split("|")
+                y = org[1]
+                for line in lines:
+                    frame = cv2.putText(frame, line, (org[0], y), font, font_scale, font_color,
+                                        thickness, cv2.LINE_AA)
+                    y += 30
+                frame = cv2.putText(frame, "prediction: " + labels[prediction], (org[0], y), font,
+                                    font_scale, (0, 255, 0), thickness, cv2.LINE_AA)
+            else:
+                frame = cv2.putText(frame, "No face detected", org, font,
+                                    font_scale, (0, 0, 255), thickness, cv2.LINE_AA)
             cv2.imshow('camera', frame)  # render frame
-
-            output = m(input_frame)
-            probability = torch.nn.functional.softmax(output, dim=1)
-            max_probability, prediction = torch.max(probability, dim=1)
-            to_decimal = ['{:.2f}'.format(float(item.item())) for item in probability[0]]
-            mapping = {labels[i]: to_decimal[i] for i in range(7)}
-            print(f"Prediction: {labels[prediction]}, probability: {mapping}")
 
     capture.release()
 
 
 if __name__ == '__main__':
-    camera()
+    camera(width=3840, high=2160)
--- code/models/VGG19.py
+++ code/models/VGG19.py
@@ -1,45 +1,49 @@
 import torch
 import torch.nn as nn
 
-model_name = 'VGG16 4096x4096x7'
+model_name = 'VGG19_4096x4096x7'
 pth_save_path = './model_data/' + model_name + '/model.pth'
 pth_manual_save_path = './model_data/' + model_name + '/manual_save_model.pth'
 record_save_path = './model_data/' + model_name
 
-# Define the VGG-16 architecture
-class VGG16(nn.Module):
+class EmotionCNN(nn.Module):
     def __init__(self, num_classes=7):
-        super(VGG16, self).__init__()
-        
+        super(EmotionCNN, self).__init__()
+
         self.features = nn.Sequential(
             nn.Conv2d(1, 64, kernel_size=3, padding=1),
             nn.ReLU(inplace=True),
             nn.Conv2d(64, 64, kernel_size=3, padding=1),
             nn.ReLU(inplace=True),
             nn.MaxPool2d(kernel_size=2, stride=2),
-            
+
             nn.Conv2d(64, 128, kernel_size=3, padding=1),
             nn.ReLU(inplace=True),
             nn.Conv2d(128, 128, kernel_size=3, padding=1),
             nn.ReLU(inplace=True),
             nn.MaxPool2d(kernel_size=2, stride=2),
-            
+
             nn.Conv2d(128, 256, kernel_size=3, padding=1),
             nn.ReLU(inplace=True),
             nn.Conv2d(256, 256, kernel_size=3, padding=1),
             nn.ReLU(inplace=True),
             nn.Conv2d(256, 256, kernel_size=3, padding=1),
             nn.ReLU(inplace=True),
+            nn.Conv2d(256, 256, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
             nn.MaxPool2d(kernel_size=2, stride=2),
-            
+
             nn.Conv2d(256, 512, kernel_size=3, padding=1),
             nn.ReLU(inplace=True),
             nn.Conv2d(512, 512, kernel_size=3, padding=1),
             nn.ReLU(inplace=True),
             nn.Conv2d(512, 512, kernel_size=3, padding=1),
             nn.ReLU(inplace=True),
+            nn.Conv2d(512, 512, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
             nn.MaxPool2d(kernel_size=2, stride=2),
-            
+            nn.Conv2d(512, 512, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
             nn.Conv2d(512, 512, kernel_size=3, padding=1),
             nn.ReLU(inplace=True),
             nn.Conv2d(512, 512, kernel_size=3, padding=1),
@@ -48,7 +52,7 @@ class VGG16(nn.Module):
             nn.ReLU(inplace=True),
             nn.MaxPool2d(kernel_size=2, stride=2)
         )
-        
+        self.avgpool = nn.AdaptiveAvgPool2d((7, 7))
         self.classifier = nn.Sequential(
             nn.Linear(512 * 7 * 7, 4096),
             nn.ReLU(inplace=True),
@@ -58,9 +62,11 @@ class VGG16(nn.Module):
             nn.Dropout(),
             nn.Linear(4096, num_classes)
         )
-    
+
     def forward(self, x):
         x = self.features(x)
+        x = self.avgpool(x)
         x = x.view(x.size(0), -1)
         x = self.classifier(x)
         return x
+
