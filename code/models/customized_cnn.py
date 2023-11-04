d1245738680a9c37c038a9a86eef03efa476cf65
williankargo <pinkuanxie@gmail.com>
2023-11-04 16:22:00 -0400

    update file name and fix graph name

--- code/models/customized_cnn.py
+++ code/models/customized_cnn.py
@@ -6,7 +6,7 @@ import torch.nn as nn
 # reference: Customized-CNN
 
 # initialize loss-function and optimizer
-model_name = 'Customized-CNN_ADAM_lr0001'
+model_name = 'Customized-cnn_ADAM_ELU_lr0001'
 pth_save_path = './model_data/' + model_name + '/model.pth'
 pth_manual_save_path = './model_data/' + model_name + '/manual_save_model.pth'
 record_save_path = './model_data/' + model_name
@@ -16,18 +16,19 @@ class EmotionCNN(nn.Module):
     def __init__(self, num_classes):
         super(EmotionCNN, self).__init__()
         self.features = nn.Sequential(
-            nn.Conv2d(in_channels=1, out_channels=64, kernel_size=3, padding=1),
+            nn.Conv2d(in_channels=1, out_channels=64,
+                      kernel_size=3, padding=1),
             # in_channels=1 -> gray channel
             # out_channels is decided by # of filters
             # batch_size doesn't show here and is different from in_channels.
-            nn.ELU(inplace=True), # inplace: override
+            nn.ELU(inplace=True),  # inplace: override
             nn.BatchNorm2d(64),
             nn.Conv2d(64, 64, kernel_size=3, padding=1),
             nn.ELU(inplace=True),
             nn.BatchNorm2d(64),
 
             nn.MaxPool2d(kernel_size=2, stride=2),
-            nn.Dropout(0.3), # get rid of 30% of data
+            nn.Dropout(0.3),  # get rid of 30% of data
 
             nn.Conv2d(64, 128, kernel_size=3, padding=1),
             nn.ELU(inplace=True),
