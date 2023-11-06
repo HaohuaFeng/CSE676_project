94b0671578fec07d5a3df95dc8bb0aa9859d68e4
Haohua Feng <haohuafng08@gmail.com>
2023-11-06 05:43:24 -0500

    add augmentation dataset functions

--- code/helper/utility.py
+++ code/helper/utility.py
@@ -2,8 +2,15 @@ import torch
 import pickle
 import matplotlib.pyplot as plt
 import numpy as np
+import random
+from torch.utils.data import DataLoader, ConcatDataset, Subset
 from sklearn.metrics import confusion_matrix, classification_report
-import scikitplot as skplt  # run "pip3 install scikit-plot" in your env first
+import scikitplot as skplt
+from collections import defaultdict, OrderedDict
+from torchvision import datasets, transforms
+import os
+import shutil
+
 
 def select_devices(use_cudnn_if_avaliable):
     if torch.cuda.is_available():
@@ -40,14 +47,11 @@ def model_validation(model, device, data_loader, pth_path, record_save_path):
     model.load_state_dict(torch.load(pth_path))
     model.to(device)
     model.eval()
-
     correct = 0
     total = 0
-
     # for confusion matrix
     y_true = []
     y_pred = []
-
     with torch.no_grad():
         for inputs, labels in data_loader:
             inputs, labels = inputs.to(device), labels.to(device)
@@ -59,10 +63,8 @@ def model_validation(model, device, data_loader, pth_path, record_save_path):
             # for confusion matrix
             y_true.extend(labels.cpu().numpy())
             y_pred.extend(predicted.cpu().numpy())
-
     accuracy = 100 * correct / total
     print(f"Test Accuracy: {accuracy}%")
-
     # calculate confusion matrix
     conf_matrix = confusion_matrix(y_true, y_pred)
     plt.figure(figsize=(7, 7))
@@ -89,3 +91,98 @@ def plot_record(x, y, xlabel, ylabel, title, save_path, show=True):
     plt.savefig(save_path)
     if show:
         plt.show()
+
+
+def combine_data(dataset1, dataset2, batch_size, shuffle=False, pin_memory=False, num_workers=0):
+    len_dataset1 = len(dataset1)
+    len_dataset2 = len(dataset2)
+    if len_dataset1 > len_dataset2:
+        larger_dataset = dataset1
+        smaller_dataset = dataset2
+    else:
+        larger_dataset = dataset2
+        smaller_dataset = dataset1
+    num_samples_to_select = len(smaller_dataset)
+    selected_samples = random.sample(range(len(larger_dataset)), num_samples_to_select)
+    selected_larger_dataset = Subset(larger_dataset, selected_samples)
+    combined_dataset = ConcatDataset([smaller_dataset, selected_larger_dataset])
+    dataloader = DataLoader(combined_dataset, batch_size=batch_size, shuffle=shuffle, 
+                            pin_memory=pin_memory, num_workers=num_workers)
+    return dataloader
+
+
+def imbalance_combine_data(datasets, batch_size, shuffle=False, pin_memory=False, num_workers=0):
+    combined_dataset = ConcatDataset(datasets)
+    dataloader = DataLoader(combined_dataset, batch_size=batch_size, shuffle=shuffle, 
+                            pin_memory=pin_memory, num_workers=num_workers)
+    return dataloader
+
+
+def check_label_size(dataloader):
+    label_counts = defaultdict(int)
+    for _, labels in dataloader:
+        labels = labels.tolist()
+        for label in labels:
+            label_counts[label] += 1
+    for label, count in label_counts.items():
+        print(f"Label {label}: {count} samples")
+    return label_counts
+
+
+def balance_weight(train_label_count, train_loader_len, batch_size):
+    class_weights = {}
+    for class_label, class_count in train_label_count.items():
+        class_weights[class_label] = (train_loader_len*batch_size / (len(train_label_count) * class_count))
+    class_weights = OrderedDict(class_weights.items())
+    print(f"adjusted weights: {class_weights}")
+    return class_weights
+
+
+def grayscale_transform_rotation(channel, size, degree):
+    transformer = transforms.Compose([
+        transforms.Grayscale(num_output_channels=channel),
+        transforms.RandomRotation(degrees=(-degree, degree)),
+        transforms.Resize(size), 
+        transforms.ToTensor(),
+    ])
+    return transformer
+
+
+def grayscale_transform_verticalflip(channel, size, p=0.5):
+    transformer = transforms.Compose([
+        transforms.Grayscale(num_output_channels=channel),
+        transforms.RandomVerticalFlip(p),
+        transforms.Resize(size), 
+        transforms.ToTensor(),
+    ])
+    return transformer
+
+
+def grayscale_transform_crop(channel, size, scale=(0.08, 1)):
+    transformer = transforms.Compose([
+        transforms.Grayscale(num_output_channels=channel),
+        transforms.RandomResizedCrop(size, scale=scale),
+        transforms.Resize(size), 
+        transforms.ToTensor(),
+    ])
+    return transformer
+
+
+def create_augmentation_dataset(source_path, target_path, class_name):
+    target_path = '../augmentation_dataset/' + target_path + '/'
+    if not os.path.exists(target_path):
+        os.mkdir(target_path)
+        for class_ in os.listdir(source_path):
+            target_class_path = target_path + class_ + '/'
+            s = source_path + '/' + class_ + '/'
+            if class_name == class_:
+                shutil.copytree(s, target_class_path)
+            elif not os.path.exists(target_class_path):
+                os.makedirs(target_class_path)
+                any_file = os.listdir(s)[0]
+                shutil.copy(s+any_file, target_class_path)
+        print(f'data augmentation dataset [{class_name}] created successfuly at [{target_class_path}]')
+    else:
+        print(f'data augmentation is already created at [{target_path}]')
+        
+    
