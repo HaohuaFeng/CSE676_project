ba1d5e29097cb13e6b9d7d6a26d6be518995a65b
Haohua Feng <haohuafeng08@gmail.com>
2023-11-12 10:12:47 -0500

    add check loss value

--- code/helper/training_early_stop.py
+++ code/helper/training_early_stop.py
@@ -4,43 +4,57 @@ class EarlyStop:
     """use to determine early stop
         >>> EarlyStop(path to store model, stop count, different, type  = "loss" or "accuracy")
     """
-    def __init__(self, path, stop_count, diff, type = "loss"):
+    def __init__(self, path, loss_path, stop_count, diff, diff_loss, type = "loss"):
         self.path = path
+        self.loss_path = loss_path
         self.stop_count = stop_count
         self.counter = 0
         self.different = diff
+        self.loss_different = diff_loss
         self.best_of_all_value = float('inf')
+        self.best_of_all_value_loss = float('inf')
         self.early_stop = False
         self.best_value = None
+        self.best_value_loss = None
         self.type = type
 
     def save(self, model, value):
         self.best_of_all_value = value
         torch.save(model.state_dict(), self.path)
 
-    def check_status(self, model, value):
-        if self.best_value is None:
+    def save_loss(self, model, value):
+        self.best_of_all_value_loss = value
+        torch.save(model.state_dict(), self.loss_path)
+
+    def check_status(self, model, value, loss_value):
+        if self.best_value is None and self.best_value_loss is None:
             self.counter = 0
             self.best_value = value
+            self.best_value_loss = loss_value
             self.save(model, value)
+            self.save_loss(model, loss_value)
             self.early_stop = False
-        elif self.type == "loss" and value > self.best_value - self.different:
-            self.counter += 1
-            if value < self.best_value:
-                self.save(model, value)
-            if self.counter >= self.stop_count:
-                self.early_stop = True
-                # self.best_value = None
-                self.counter = 0
-        elif self.type == "accuracy" and value < self.best_value + self.different:
-            self.counter += 1
-            if value > self.best_value:
-                self.save(model, value)
-            if self.counter >= self.stop_count:
-                self.early_stop = True
-                # self.best_value = None
-                self.counter = 0
         else:
-            self.best_value = value
-            self.save(model, value)
-            self.counter = 0
+            if  loss_value > self.best_value_loss - self.loss_different:
+                if self.type == "loss":
+                    self.counter += 1
+                if self.counter >= self.stop_count:
+                    self.early_stop = True
+                    self.counter = 0
+            else:
+                self.best_value_loss = loss_value
+                self.save_loss(model, loss_value)
+                if self.type == "loss":
+                    self.counter = 0
+
+            if  value < self.best_value + self.different:
+                if self.type == "accuracy":
+                    self.counter += 1
+                if self.counter >= self.stop_count:
+                    self.early_stop = True
+                    self.counter = 0
+            else:
+                self.best_value = value
+                self.save(model, value)
+                if self.type == "accuracy":
+                    self.counter = 0
